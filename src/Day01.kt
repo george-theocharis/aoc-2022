10b11cc57dcfcd34ed8f7bf6ddf5708ee093a117
George Theocharis <giwrgos.theocharis@gmail.com>
2022-12-01 17:14:00 +0200

    Solve day 1 aoc

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,32 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int =
+        input
+            .fold(mutableListOf(0)) { acc: MutableList<Int>, item ->
+                if (item.isNotEmpty()) {
+                    acc[acc.lastIndex] = acc[acc.lastIndex] + item.toInt()
+                } else {
+                    acc.add(0)
+                }
+                acc
+            }
+            .max()
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part2(input: List<String>): Int = input
+        .fold(mutableListOf(0)) { acc: MutableList<Int>, item ->
+            if (item.isNotEmpty()) {
+                acc[acc.lastIndex] = acc[acc.lastIndex] + item.toInt()
+            } else {
+                acc.add(0)
+            }
+            acc
+        }
+        .sortedDescending()
+        .take(3)
+        .sum()
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+
 }
